d149312e39d82ca3d785e3e1e9d74d9756f0aa8d
Aashish Prajapati <148204071+aashuftech@users.noreply.github.com>
2026-02-12 10:42:21 +0530

    add frontend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import AuthorList from "./components/AuthorsList";
+
+function App(){
+    return (
+        <div style={{ padding: "20px"}}>
+            <h1>Author Royalty System</h1>
+            <AuthorList/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/AuthorsList.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+const API = "http://localhost:2000";
+
+function AuthorList() {
+    
+    const [authors, setAuthors] = useState([]);
+
+    useEffect(()=>{
+        fetch(`${API}/authors`).then(res => res.json())
+        .then(data => setAuthors(data));
+
+    },[]);
+
+    return(
+        <div>
+            <h1>Authors</h1>
+
+            {authors.map(a => (
+                <div key={a.id}>
+                    <h3>{a.name}</h3>
+                    <p>Total Earnings: ₹{a.total_earnings}</p>
+                    <p>Balance: ₹{a.current_balance}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default AuthorList;
